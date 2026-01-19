5f422e3ec349a7b759bc63f671ac24f7c22d4ba8
estban03 <estebanmutuverria003@gmail.com>
2026-01-18 23:51:31 -0300

    menu responsive added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import './Navbar.css';
-
 import SpinningText from '../SpinningText/SpinningText';
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
     const [scrolled, setScrolled] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
     useEffect(() => {
         const handleScroll = () => {
@@ -15,22 +16,35 @@ const Navbar = () => {
         return () => window.removeEventListener('scroll', handleScroll);
     }, []);
 
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const closeMenu = () => {
+        setIsOpen(false);
+    };
+
     return (
         <nav className={`navbar ${scrolled ? 'scrolled' : ''}`}>
             <div className="navbar-container">
-                <a href="#" className="logo-container">
+                <a href="#" className="logo-container" onClick={closeMenu}>
                     <SpinningText radius={2} fontSize={0.6} className='font-medium leading-none'>
                         {`Esteban • Mutuverria • `}
                     </SpinningText>
                 </a>
-                <ul className="nav-links">
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#skills">Skills</a></li>
-                    <li><a href="#projects">Projects</a></li>
-                    <li><a href="#experience">Experience</a></li>
-                    <li><a href="#education">Education</a></li>
-                    <li><a href="#contact">Contact</a></li>
+
+                <div className="menu-icon" onClick={toggleMenu}>
+                    {isOpen ? <FaTimes /> : <FaBars />}
+                </div>
+
+                <ul className={`nav-links ${isOpen ? 'active' : ''}`}>
+                    <li><a href="#home" onClick={closeMenu}>Home</a></li>
+                    <li><a href="#about" onClick={closeMenu}>About</a></li>
+                    <li><a href="#skills" onClick={closeMenu}>Skills</a></li>
+                    <li><a href="#projects" onClick={closeMenu}>Projects</a></li>
+                    <li><a href="#experience" onClick={closeMenu}>Experience</a></li>
+                    <li><a href="#education" onClick={closeMenu}>Education</a></li>
+                    <li><a href="#contact" onClick={closeMenu}>Contact</a></li>
                 </ul>
             </div>
         </nav>
